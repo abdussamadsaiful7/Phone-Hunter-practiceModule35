306341039e32d12bbb12c1b9d4d35ac56dd553e1
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-26 17:33:06 +0600

    added togglespinner

--- js/phone.js
+++ js/phone.js
@@ -36,13 +36,27 @@ const displayPhones = phones =>{
         `;
         phonesContainer.appendChild(phoneDiv);
     })
+    // stop toggle spinner,
+    toggleSpinner(false);
 }
 
 document.getElementById('btn-search').addEventListener('click', function(){
+    //start loader
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhones(searchText);
 
 })
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
 
 loadPhones('phone');
