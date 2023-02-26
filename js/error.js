613881c95d64ed15d78c923940566d6a951fe0d0
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-26 21:22:32 +0600

    edit and task completed

---
+++ js/error.js
@@ -0,0 +1,20 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(error => console.log(error));
+
+// try{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await res.json();
+//     console.log(data);
+// }
+// catch{
+
+// }
+
+
+// console.log(1);
+// setTimeout(()=>{
+//     console.log(2)
+// }, 5000)
+// console.log(12345);
