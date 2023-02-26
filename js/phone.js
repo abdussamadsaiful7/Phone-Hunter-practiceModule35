8ac05a74a1531dc7ebf626ee8e3b0f15dadd9e96
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-26 19:03:04 +0600

    added modal

--- js/phone.js
+++ js/phone.js
@@ -38,7 +38,7 @@ const displayPhones = (phones, dataLimit) =>{
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
+                <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
             </div>
         </div>
         `;
