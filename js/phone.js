ccacc751aecf36c096e6b2e93b65b2c0c0bb9763
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-26 13:18:28 +0600

    added card shawod

--- js/phone.js
+++ js/phone.js
@@ -2,11 +2,26 @@ const loadPhones =async() =>{
     const url =` https://openapi.programming-hero.com/api/phones?search=iphone`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data)
+    displayPhones(data.data);
+    console.log(data)
 }
 
 const displayPhones = phones =>{
-    console.log(phones);
+    const phonesContainer = document.getElementById('phones-container');
+    phones.forEach(phone =>{
+        const phoneDiv = document.createElement('div');
+        phoneDiv.classList.add('col');
+        phoneDiv.innerHTML = `
+        <div class="card w-full h-60 p-4 mt-4 border border-0 bg-primary-subtle shadow bg-body-tertiary rounded">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        phonesContainer.appendChild(phoneDiv);
+    })
 }
 
 loadPhones();
